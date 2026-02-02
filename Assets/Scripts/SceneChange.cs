60e313f5de4bc9f8fbfe81596e62a37816a8ecdb
tomoya <tomoyamario29@gmail.com>
2026-02-02 16:35:58 +0900

    シーン遷移を実装した。

---
+++ Assets/Scripts/SceneChange.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    private void Update()
+    {
+        Changescene();
+    }
+    void Changescene()
+    {
+        if (Input.GetKey(KeyCode.E))
+        {
+            SceneManager.LoadScene("New Scene");
+        }
+        
+    }
+
+}
